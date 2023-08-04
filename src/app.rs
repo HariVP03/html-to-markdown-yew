0271b98fbf2d5e1fb8b17fe48eacd42a761748ec
Hari Vishnu Parashar <harryskotch11@gmail.com>
2023-08-04 11:03:47 +0530

    Init some setup and tauri app

---
+++ src/app.rs
@@ -0,0 +1,10 @@
+use yew::prelude::*;
+
+use crate::components::layout::Layout;
+
+#[function_component(App)]
+pub fn app() -> Html {
+    html! {
+        <Layout />
+    }
+}
---
+++ src/components/layout.rs
@@ -0,0 +1,10 @@
+use yew::prelude::*;
+
+#[function_component(Layout)]
+pub fn layout() -> Html {
+    html! {
+        <div>
+        {"123"}
+        </div>
+    }
+}
